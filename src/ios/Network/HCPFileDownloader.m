4452173ddcf90efe47c8972c2972aeedc23f4cc7
Be4t5 <thomas.traverso54@gmail.com>
2017-04-24 20:31:00 +0200

    Update HCPFileDownloader.m

--- src/ios/Network/HCPFileDownloader.m
+++ src/ios/Network/HCPFileDownloader.m
@@ -107,14 +107,14 @@ static NSUInteger const TIMEOUT = 300;
  *  @return <code>YES</code> if file is corrupted; <code>NO</code> if file is valid
  */
 - (BOOL)isFileCorrupted:(NSURL *)file checksum:(NSString *)checksum {
-    NSString *dataHash = [[NSData dataWithContentsOfURL:file] md5];
-    /*if ([dataHash isEqualToString:checksum]) {
+   /* NSString *dataHash = [[NSData dataWithContentsOfURL:file] md5];
+    if ([dataHash isEqualToString:checksum]) {
         return NO;
-    }*/
+    }
     
     NSLog(@"Hash %@ doesn't match the checksum %@", dataHash, checksum);
-    
-    return YES;
+    */
+    return NO;
 }
 
 /**
